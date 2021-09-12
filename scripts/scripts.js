7a0925d207010d4ee153207e90e049405c60e95e
Saul Escobar <jescobarg9@miumg.edu.gt>
2021-09-11 19:40:31 -0600

    reorganización de paginas, actualización css y creacion del js. Se puede visualizar su uso en el tema css

---
+++ scripts/scripts.js
@@ -0,0 +1,11 @@
+function cambiarFrame(direccion, id) {
+    document.getElementById('iframe-contenedor').src = direccion;
+    document.getElementById(id).className = "pagina-activa";
+};
+
+function resetearlinksActivos(link1, link2, link3, link4) {
+    document.getElementById(link1).className = "";
+    document.getElementById(link2).className = "";
+    document.getElementById(link3).className = "";
+    document.getElementById(link4).className = "";
+};
